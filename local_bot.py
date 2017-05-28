2a5dcfa15670d10abf2b376ad63afa4d0f5d9c17
Michael Meneghini <michael98menega@gmail.com>
2017-05-28 11:35:05 +0200

    little fixes

--- local_bot.py
+++ local_bot.py
@@ -3,9 +3,14 @@ from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 import telepot
 import time
 
+chosen_skill= "";
+
 def handle(msg):
     text = msg["text"]
     chat_id = msg["chat"]["id"]
+
+    # variable that I need to store the skill that user wantsm in order to retrieve the right level routine for THAT skill
+    #chosen_skill = ""
 
     if text == "/test":
         bot.sendMessage(chat_id, "The bot is responding!")
@@ -14,7 +19,7 @@ def handle(msg):
     elif text == "/routines":
         #matrix for keyboard buttons
         #TODO: add other skills
-        #TODO: keep updated the skills list and the handleSkillRequest method too!
+        #TODO: keep updated the skills list too!
         skills_keyboard = [[KeyboardButton(text="Basics")],[KeyboardButton(text="Front Lever")], [KeyboardButton(text="Planche")]]
         bot.sendMessage(chat_id, "Choose a skill", reply_markup=ReplyKeyboardMarkup(keyboard=skills_keyboard))
 
@@ -22,9 +27,12 @@ def handle(msg):
         chosen_skill = text
         levels_keyboard = [[KeyboardButton(text="Beginner")], [KeyboardButton(text="Intermediate")],[KeyboardButton(text="Advanced")]]
         bot.sendMessage(chat_id, "Choose your level", reply_markup=ReplyKeyboardMarkup(keyboard=levels_keyboard))
+        print(chosen_skill)
 
-    elif text in levels:
-        handleLevelRequest(text, chat_id)
+    elif text in levels:\
+        #bot.sendMessage(chat_id, "{} routine for {}".format(text, chosen_skill))
+        bot.sendDocument(chat_id,open("{}_{}_routine".format(chosen_skill,text)))
+        chosen_skill = ""
 
     # default echo response
     else:
@@ -32,25 +40,11 @@ def handle(msg):
         return "OK"
 
 
-def handleLevelRequest(level, chat):
-    if level == "Beginner":
-        bot.sendMessage(chat, "Beginner routine for chosen skill")
-        #send beginner routine for chosen routine
-    elif level == "Intermediate":
-        bot.sendMessage(chat, "Intermediate routine for chosen skill")
-    #advanced
-    else:
-        bot.sendMessage(chat, "Advanced routine for chosen skill")
-    chosen_skill = ""
-
-
 #skills list that I'll need later
 skills = ["Basics","Front Lever","Planche"]
 
 levels = ["Beginner","Intermediate","Advanced"]
 
-#variable that I need to store the skill that user wantsm in order to retrieve the right level routine for THAT skill
-chosen_skill = ""
 
 bot = telepot.Bot('343392414:AAECRTAnbVpxr9__x-gdfCUpwwIf3pAcLJs')
 
